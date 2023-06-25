673d1fd5c636a2a21cab220e8f50c50be6129da0
MyCode07 <erem19992015@mail.ru>
2023-06-25 14:12:14 +0300

    finished main layout

--- src/js/app.js
+++ src/js/app.js
@@ -5,8 +5,8 @@ import './utils/sliders.js';
 
 // import './utils/mouse.js';
 // import './utils/ticker.js';
-// import './utils/to-top.js';
-// import './utils/popup.js';
+import './utils/to-top.js';
+import './utils/popup.js';
 // import './utils/inputmask.js';
 // import './utils/forms.js';
 import './utils/accordeon.js';
--- src/js/utils/popup.js
+++ src/js/utils/popup.js
@@ -1,9 +1,7 @@
-import { isMobile } from "./isMobile.js";
-
 const popup = document.querySelector('.popup');
 const popupClose = document.querySelector('.popup__close');
-const popupOpenButtons = document.querySelectorAll('.popup-open');
-
+// const popupOpenButtons = document.querySelectorAll('.popup-open');
+const popupOpenButtons = document.querySelectorAll('.play');
 
 if (popupOpenButtons.length) {
     popupOpenButtons.forEach(btn => {
@@ -11,39 +9,18 @@ if (popupOpenButtons.length) {
             e.preventDefault();
             popup.classList.add('_open');
             document.body.classList.add('_noscroll');
-
-            if (!isMobile.any()) {
-                lockPadding()
-            }
         })
 
         popupClose.addEventListener('click', function () {
             popup.classList.remove('_open');
             document.body.classList.remove('_noscroll');
-
-            if (!isMobile.any()) {
-                unLockPadding()
-            }
         })
 
         popup.addEventListener('click', function (e) {
             if (e.target.classList.contains('popup')) {
                 popup.classList.remove('_open')
                 document.body.classList.remove('_noscroll');
-                unLockPadding()
             }
         })
     })
-}
-
-
-function lockPadding() {
-    const paddingRight = 10; // scrollbarwidth
-    document.body.style.paddingRight = paddingRight + 'px';
-}
-
-
-function unLockPadding() {
-    const paddingRight = 10; // scrollbarwidth
-    document.body.style.paddingRight = 0 + 'px';
 }
