a3a7d815f6d68924f5367cdffc76354aee69327a
MyCode07 <erem19992015@mail.ru>
2023-06-28 10:55:56 +0300

    added loading games to wp, change popup game best casino layout

--- src/js/utils/popup.js
+++ src/js/utils/popup.js
@@ -1,12 +1,22 @@
 const popup = document.querySelector('.popup');
-const popupClose = document.querySelector('.popup__close');
-// const popupOpenButtons = document.querySelectorAll('.popup-open');
-const popupOpenButtons = document.querySelectorAll('.play');
+const popupClose = popup.querySelector('.popup__close');
+const popupFullScreen = popup.querySelector('.popup__header-fullscreen');
+const popupOpenButtons = document.querySelectorAll('.open-popup');
+const popupTitle = popup.querySelector('.popup__header h4');
+const popupGame = popup.querySelector('.popup__body');
+const popupFooter = document.querySelector('.popup__footer');
+const popupFooterToggle = document.querySelector('.popup__footer-toggle');
+
 
 if (popupOpenButtons.length) {
     popupOpenButtons.forEach(btn => {
+        const game = btn.closest('.casino-slide');
+        const gameTitle = game.querySelector('[data-title]').textContent;
+
         btn.addEventListener('click', function (e) {
             e.preventDefault();
+
+            popupTitle.textContent = gameTitle;
             popup.classList.add('_open');
             document.body.classList.add('_noscroll');
         })
@@ -14,6 +24,10 @@ if (popupOpenButtons.length) {
         popupClose.addEventListener('click', function () {
             popup.classList.remove('_open');
             document.body.classList.remove('_noscroll');
+
+            if (popupGame.querySelector('iframe')) {
+                popupGame.querySelector('iframe').remove();
+            }
         })
 
         popup.addEventListener('click', function (e) {
@@ -22,5 +36,42 @@ if (popupOpenButtons.length) {
                 document.body.classList.remove('_noscroll');
             }
         })
+    })
+}
+
+if (popupFullScreen) {
+    popupFullScreen.addEventListener('click', function (e) {
+        e.preventDefault();
+        const iframe = popupGame.querySelector('iframe');
+
+        openFullscreen(iframe)
+    })
+}
+
+function openFullscreen(iframe) {
+    if (iframe.requestFullscreen) {
+        iframe.requestFullscreen();
+    } else if (iframe.webkitRequestFullscreen) { /* Safari */
+        iframe.webkitRequestFullscreen();
+    } else if (iframe.msRequestFullscreen) { /* IE11 */
+        iframe.msRequestFullscreen();
+    }
+}
+
+function closeFullscreen() {
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) { /* Safari */
+        document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) { /* IE11 */
+        document.msExitFullscreen();
+    }
+}
+
+
+if (popupFooterToggle) {
+    popupFooterToggle.addEventListener('click', function (e) {
+        popupFooterToggle.classList.toggle('_active')
+        popupFooter.classList.toggle('_active')
     })
 }
